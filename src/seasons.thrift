b08f4e7643760043fc333c934d218527d0d81e29
millcityrunner <mcaray9@gmail.com>
2023-03-02 20:54:25 -1000

    Adding in seasons thrift changes

--- src/seasons.thrift
+++ src/seasons.thrift
@@ -16,18 +16,87 @@ enum SeasonStatus {
 // structs
 struct Season {
     1: optional common.SeasonId id;
-    2: optional common.Date start_date;
-    3: optional common.Date end_date;
-    4: optional SeasonStatus status;
-    5: optional common.Timestamp created_ts;
-    6: optional common.Timestamp updated_ts;
+    2: optional common.Timestamp start_ts;
+    3: optional common.Timestamp end_ts;
+    4: optional common.Timestamp created_ts;
+    5: optional common.Timestamp updated_ts;
+    6: optional bool is_active;
+    7: optional common.SportId sport_id;
 }
 
 struct SeasonResponse {
     1: optional list<Season> seasons;
 }
 
+struct CreateSeasonRequest {
+    1: optional common.Timestamp start_ts;
+    2: optional common.Timestamp created_ts;
+    3: optional bool is_active;
+    4: optional common.SportId sport_id;
+    5: optional common.Timestamp end_ts;
+    6: optional common.Timestamp updated_ts;
+}
+
+struct CreateSeasonResponse {
+    1: optional Season season;
+}
+
+struct GetSeasonsRequest {
+    1: optional common.SportId sport_id;
+    2: optional bool is_active;
+    3: optional common.Timestamp created_after_ts;
+    4: optional common.Timestamp updated_after_ts;
+    5: optional common.Timestamp start_after_ts;
+    6: optional common.Timestamp end_after_ts;
+    7: optional common.Timestamp created_before_ts;
+    8: optional common.Timestamp updated_before_ts;
+    9: optional common.Timestamp start_before_ts;
+    10: optional common.Timestamp end_before_ts;
+    11: optional common.SeasonId season_id;
+}
+
+struct GetSeasonsResponse {
+    1: optional list<Season> seasons;
+}
+
+struct UpdateSeasonsRequest {
+    1: optional common.SeasonId season_id;
+    2: optional common.Timestamp created_ts;
+    3: optional common.Timestamp updated_ts;
+    4: optional common.Timestamp start_ts;
+    5: optional common.Timestamp end_ts;
+    6: optional bool is_active;
+    7: optional common.SportId sport_id;
+}
+
+struct DeleteSeasonRequest {
+    1: optional common.SeasonId season_id;
+}
 
 service SeasonService {
-    SeasonResponse get_seasons();
+    GetSeasonsResponse get_seasons(
+        1: GetSeasonsRequest request
+    ) throws (
+        1: common.NotFound not_found,
+        2: common.InvalidRequest invalid_request,
+    );
+
+    CreateSeasonResponse create_season(
+        1: CreateSeasonRequest request
+    ) throws (
+        1: common.ResourceAlreadyExists resource_already_exists,
+        2: common.NotFound not_found,
+    );
+
+    common.Response update_season(
+        1: UpdateSeasonRequest request
+    ) throws (
+        1: common.NotFound not_found;
+    );
+
+    common.Response delete_season(
+        1: DeleteSeasonRequest request
+    ) throws (
+        1: common.NotFound not_found,
+    );
 }
