b7403527fc3d883b0546291934b267509498a12c
millcityrunner <mcaray9@gmail.com>
2023-03-16 05:24:33 -0500

    Adding in fields for conference

--- src/conferences.thrift
+++ src/conferences.thrift
@@ -6,22 +6,14 @@ include "seasons.thrift"
 
 // typedefs
 
-// enums
-enum ConferenceStatus {
-    ACTIVE = 1,
-    INACTIVE = 2,
-    TERMINATED = 3,
-}
-
 // structs
 struct Conference {
     1: optional common.ConferenceId id;
     2: optional string name;
-    3: optional ConferenceStatus status;
-    4: optional list<common.Year> years_active;
-    5: optional common.SeasonId season_id;
-    6: optional common.Timestamp created_ts;
-    7: optional common.Timestamp updated_ts;
+    3: optional bool is_active;
+    4: optional common.SeasonId season_id;
+    5: optional common.Timestamp created_ts;
+    6: optional common.Timestamp updated_ts;
 }
 
 struct ConferenceResponse {
