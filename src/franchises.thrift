8130072e4ccbf363ae06266a08bd2334f68ba943
millcityrunner <mcaray9@gmail.com>
2023-04-22 22:34:23 -0500

    adding in franchises to thrift

--- src/franchises.thrift
+++ src/franchises.thrift
@@ -16,25 +16,86 @@ enum FranchiseStatus {
 }
 
 // structs
-struct Tenure {
-    1: optional common.Year start_year;
-    2: optional common.Year end_year;
-}
-
 struct Franchise {
     1: optional common.FranchiseId id;
-    2: optional common.Timestamp established_date;
+    2: optional common.Timestamp created_ts;
     3: optional common.TeamId root_team_id;
-    4: optional map<common.TeamId, Tenure> lineage;
-    5: optional common.Timestamp created_ts;
+    4: optional list<common.TeamId> teams;
+    5: optional bool is_active;
     6: optional common.Timestamp updated_ts;
-    7: optional common.SportId sport_id;
+    7: optional FranchiseStatus status;
 }
 
 struct FranchiseResponse {
     1: optional list<Franchise> franchises;
 }
 
+struct CreateFranchiseRequest {
+    1: optional common.Timestamp created_ts;
+    2: optional common.Timestamp updated_ts;
+    3: optional bool is_active;
+    4: optional common.TeamId root_team_id;
+    5: optional list<common.TeamId> teams;
+    6: optional FranchiseStatus status;
+}
+
+struct CreateFranchiseResponse {
+    1: optional Franchise franchise;
+}
+
+struct GetFranchisesRequest {
+    1: optional common.FranchiseId franchise_id;
+    2: optional bool is_active;
+    3: optional common.Timestamp created_on_or_after_ts;
+    4: optional common.Timestamp updated_on_or_after_ts;
+    5: optional common.Timestamp created_before_ts;
+    6: optional common.Timestamp updated_before_ts;
+    7: optional common.TeamId root_team_id;
+    8: optional list<common.TeamId> teams;
+    9: optional FranchiseStatus status;
+}
+
+struct GetFranchisesResponse {
+    1: optional list<Franchise> franchises;
+}
+
+struct UpdateFranchisesRequest {
+    1: optional common.FranchiseId franchise_id;
+    2: optional bool is_active;
+    3: optional common.Timestamp created_on_or_after_ts;
+    4: optional common.Timestamp updated_on_or_after_ts;
+    5: optional common.Timestamp created_before_ts;
+    6: optional common.Timestamp updated_before_ts;
+    7: optional common.TeamId root_team_id;
+    8: optional list<common.TeamId> teams;
+    9: optional FranchiseStatus status;
+}
+
+struct DeleteFranchiseRequest {
+    1: optional common.FranchiseId franchise_id;
+}
+
 service FranchiseService {
-    FranchiseResponse get_franchises();
+    GetFranchisesResponse get_franchises();
+
+    CreateFranchiseResponse create_franchise(
+        1: CreateFranchiseRequest request,
+    ) throws (
+        1: common.InvalidRequest invalid_request,
+        2: common.AlreadyExists already_exists,
+        4: common.TeamNotFoundError team_not_found,
+    );
+
+    common.Response update_franchises(
+        1: UpdateFranchisesRequest request,
+    ) throws (
+        1: common.InvalidRequest invalid_request,
+    );
+
+    common.Response delete_franchise(
+        1: DeleteFranchiseRequest request,
+    ) throws (
+        1: common.InvalidRequest invalid_request,
+        2: common.FranchiseNotFoundError franchise_not_found,
+    );
 }
